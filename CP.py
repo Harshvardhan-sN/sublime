698f8d072064be550fdce6e1d01c9b5b2db34098
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-01 08:59:22 +0530

    Add files via upload

---
+++ CP.py
@@ -0,0 +1,11 @@
+import re
+import sys
+from traceback import print_tb
+def gt(): return map(int, sys.stdin.readline().strip().split())
+def read(): return list(map(int,input().split()))
+
+t = int(input())
+while t>0:
+
+
+    t -= 1
